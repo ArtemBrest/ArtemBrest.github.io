8dfc1b854506e537167614e0edfd72e1fe25d6ad
ArtemBrest <artoym.zan@gmail.com>
2019-12-23 14:18:45 +0300

    1

---
+++ js/script.js
@@ -0,0 +1,13 @@
+$(document).ready(function () {
+    var link  = $('.menu-link');
+    var menu = $('.menu');
+    var nav = $('.menu a');
+    link.click(function () {
+        link.toggleClass("menu-link_active")
+        menu.toggleClass("menu_active")
+    })
+    nav.click(function () {
+        link.toggleClass("menu-link_active")
+        menu.toggleClass("menu_active")
+    })
+});
