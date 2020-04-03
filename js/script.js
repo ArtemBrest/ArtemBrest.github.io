653b8514b43cfb12aad6443509ea01877959f73b
ArtemBrest <artoym.zan@gmail.com>
2020-04-03 18:33:23 +0300

    test

--- js/script.js
+++ js/script.js
@@ -1,28 +1,6 @@
 $(document).ready(function () {
-
-   /* accordion*/
-    var accord = $(".accordion");
-    var up = $(".icon");
-    accord.find(".title:not(.active)").siblings("div").slideUp();
-    accord.find(".title").on("click", function () {
-        $(this).siblings("div").stop().slideToggle(500);
-
-        if ($(this).hasClass("active")) {
-            $(this).removeClass("active");
-        } else {
-            $(this).addClass("active");
-        }
-        if ($(this).find(".icon").hasClass("icon_active")) {
-            $(this).find(".icon").removeClass("icon_active");
-        } else {
-            $(this).find(".icon").addClass("icon_active");
-        }
-    });
-    /*end accordion*/
-
-
      /*Mob menu*/
-    var link = $('.menu-link');
+   /* var link = $('.menu-link');
     var menu = $('.menu');
     var nav = $('.menu a');
     link.click(function () {
@@ -32,221 +10,20 @@ $(document).ready(function () {
     nav.click(function () {
         link.toggleClass("menu-link_active")
         menu.toggleClass("menu_active")
-    })
+
+
+    })*/
+
+
+    $(".menu-link").click(function() {
+        $(".headerMenu_mobile").slideDown(300);
+    });
+
+    $(".menu-link_active").click(function() {
+        $(".headerMenu_mobile").slideUp(300);
+    });
     /* end Mob menu*/
 
 
-    /*Modal Window*/
-    var link = $('.seo_btn');
-    var modal = $('.modal2');
-    var close = $('.modal2-close');
-    var close_btn = $('.btn-close');
-    var fade = $('.fade2');
-    link.click(function () {
-        modal.show();
-        fade.show();
-    })
-    close.click(function () {
-        modal.hide();
-        fade.hide();
-    })
-    close_btn.click(function () {
-        modal.hide();
-        fade.hide();
-    })
-    fade.click(function () {
-        modal.hide();
-        fade.hide();
-    })
-    var link_2 = $('.card_btn');
-    var link_3 = $('.portfolio_btn');
-    var modal_2 = $('.modal3');
-    var close_2 = $('.modal3-close');
-    var fade_2 = $('.fade3');
-    link_2.click(function () {
-        modal_2.show();
-        fade_2.show();
-    })
-    link_3.click(function () {
-        modal_2.show();
-        fade_2.show();
-    })
-    close_2.click(function () {
-        modal_2.hide();
-        fade_2.hide();
-    })
-    fade_2.click(function () {
-        modal_2.hide();
-        fade_2.hide();
-    })
 
-    var link_4 = $('.questions_btn');
-    var link_5 = $('.progress_btn');
-    var link_6 = $('.banner_btn_price');
-    var modal_4 = $('.modal4');
-    var close_4 = $('.modal4-close');
-    var fade_4 = $('.fade4');
-    link_4.click(function () {
-        modal_4.show();
-        fade_4.show();
-    })
-    link_5.click(function () {
-        modal_4.show();
-        fade_4.show();
-    })
-    link_6.click(function () {
-        modal_4.show();
-        fade_4.show();
-    })
-    close_4.click(function () {
-        modal_4.hide();
-        fade_4.hide();
-    })
-    fade_4.click(function () {
-        modal_4.hide();
-        fade_4.hide();
-    })
-
-    /* end Modal Window*/
-
-
-
-    /*validate Form*/
-    $('.form').validate({
-        rules: {
-            name: {
-                required: true,
-                minlength: 2
-            },
-            phone: {
-                required: true,
-                minlength: 7,
-            },
-
-        },
-        messages: {
-            name: {
-                required: '*',
-                minlength: ' '
-            },
-            phone: {
-                required: '*',
-                minlength: ' ',
-                address: ' '
-            },
-
-        }
-    });
-    $('.banner_form').validate({
-        rules: {
-            name: {
-                required: true,
-                minlength: 2
-            },
-            phone: {
-                required: true,
-                minlength: 7,
-            },
-
-        },
-        messages: {
-            name: {
-                required: 'Это поле обязательное',
-                minlength: ' '
-            },
-            phone: {
-                required: 'Это поле обязательное',
-                minlength: ' ',
-                address: ' '
-            },
-
-        }
-    });
-    $('.modal2_form').validate({
-        rules: {
-            name: {
-                required: true,
-                minlength: 2
-            },
-            phone: {
-                required: true,
-                minlength: 7,
-            },
-
-        },
-        messages: {
-            name: {
-                required: '*',
-                minlength: ' '
-            },
-            phone: {
-                required: '*',
-                minlength: ' ',
-                address: ' '
-            },
-
-        }
-    });
-    $('.modal3_form').validate({
-        rules: {
-            name: {
-                required: true,
-                minlength: 2
-            },
-            phone: {
-                required: true,
-                minlength: 7,
-            },
-
-        },
-        messages: {
-            name: {
-                required: '*',
-                minlength: ' '
-            },
-            phone: {
-                required: '*',
-                minlength: ' ',
-                address: ' '
-            },
-
-        }
-    });
-    $('.modal4_form').validate({
-        rules: {
-            name: {
-                required: true,
-                minlength: 2
-            },
-            phone: {
-                required: true,
-                minlength: 7,
-            },
-
-        },
-        messages: {
-            name: {
-                required: '*',
-                minlength: ' '
-            },
-            phone: {
-                required: '*',
-                minlength: ' ',
-                address: ' '
-            },
-
-        }
-    });
-    /* end validate Form*/
-
-
-   /* maskediput*/
-    jQuery(function ($) {
-        $("#modal2_tel").mask("+375(99) 999-99-99");
-        $("#modal3_tel").mask("+375(99) 999-99-99");
-        $("#modal4_tel").mask("+375(99) 999-99-99");
-        $("#phone").mask("+375(99) 999-99-99");
-        $("#contactForm_phone").mask("+375(99) 999-99-99");
-    });
-    /* end maskediput*/
 });
