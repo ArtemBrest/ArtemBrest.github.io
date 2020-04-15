465f0957c11a46cdde476ba070835aa072e79ffc
ArtemBrest <artoym.zan@gmail.com>
2020-04-15 15:25:46 +0300

    1

--- js/script.js
+++ js/script.js
@@ -1,21 +1,21 @@
 $(document).ready(function () {
 
     /* calculator*/
-    $('body').on("keyup", 'input[type=text]', function (event) {
+   /* $('body').on("keyup", 'input[type=text]', function (event) {
         var v1 = parseFloat($(".value1").val());
         var v2 = parseFloat($(".value2").val());
         console.log(v1+ "  +  "+v2);
         $(".sum").html(v1 + v2);
 
-    });
+    });*/
     $('.minus').click(function () {
         var $input = $(this).parent().find('input');
         var count = parseInt($input.val()) - 1;
         count = count < 1 ? 1 : count;
         $input.val(count);
         $input.change();
-        var v1 = parseFloat($(".value1").val());
-        var v2 = parseFloat($(".value2").val());
+        var v1 = parseFloat($(this).prev(".value1").val());
+        var v2 = parseFloat($(this).prev(".value2").val());
         console.log(v1+ "  -  "+v2);
         $(".sum").html(v1 + v2);
         return false;
@@ -25,8 +25,8 @@ $(document).ready(function () {
         var $input = $(this).parent().find('input');
         $input.val(parseInt($input.val()) + 1);
         $input.change();
-        var v1 = parseFloat($(".value1").val());
-        var v2 = parseFloat($(".value2").val());
+        var v1 = parseFloat($(this).next(".value1").val());
+        var v2 = parseFloat($(this).next(".value2").val());
         console.log(v1+ "  +  "+v2);
         $(".sum").html(v1 + v2);
         return false;
@@ -44,7 +44,7 @@ $(document).ready(function () {
     });
     /* end banner slick*/
 
-
+    /* comment slider*/
     $('.sl_Main').slick({
         slidesToShow: 1,
         slidesToScroll: 1,
@@ -61,6 +61,21 @@ $(document).ready(function () {
         dots: false,
         focusOnSelect: true
     });
+    /* end comment slider*/
+
+    /* comment*/
+    $(".description_table_link").click(function () {
+        var block =$(".hide_description");
+        if(block.hasClass("active")){
+            block.removeClass("active");
+            block.slideToggle(500);
+        }
+        else{
+            block.addClass("active");
+            block.slideToggle(500);
+        }
+    })
+    /* end comment*/
 
 
     /* accordion*/
@@ -83,11 +98,15 @@ $(document).ready(function () {
     });
     /*end accordion*/
 
-
+   /* lamp slider*/
     $('.fixtures_slider').slick({
         slidesToShow: 4,
         slidesToScroll: 2,
         dots: true,
     });
+    /* end lamp slider*/
+
+
+
 
 })
