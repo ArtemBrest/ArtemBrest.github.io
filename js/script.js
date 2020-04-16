0d66aee46bf0c10e852cb882c2a08167a2c40a81
ArtemBrest <artoym.zan@gmail.com>
2020-04-16 23:13:48 +0300

    1

--- js/script.js
+++ js/script.js
@@ -7,26 +7,25 @@ $(document).ready(function () {
         autoplay: true,
         autoplaySpeed: 2000,
     });
+    $('.slider_calculator').slick({
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        vertical:true,
+        autoplay: true,
+        autoplaySpeed: 2000,
+    });
     /* end big menu slider*/
 
     /* big menu*/
     $(".big_menu").mouseover(function () {
-        //действия
         $(".header_menu_first_li").slideToggle(500);
-
     });
 
     $(".big_menu").mouseout(function () {
         //действия
         $(".header_menu_first_li").slideToggle(500);
     });
-    $(".header_menu_first_li").mouseout(function () {
-        //действия
-        $(this).fadeIn();
-    });
-    $(".header_menu_first_li").mouseover(function () {
-        $(this).fadeOut();
-    });
+
     /*  end big menu*/
 
 
@@ -37,7 +36,7 @@ $(document).ready(function () {
          var v3 = parseFloat($(".value3").val());
          var v4 = parseFloat($(".value4").val());
          console.log(v1+ "  +  "+v2);
-         $(".sum").html(v1 + v2 + v3 + v4 +" p.");
+         $(".sum").val(v1 + v2 + v3 + v4 +" p.");
 
      });
     $('.minus').click(function () {
@@ -51,7 +50,7 @@ $(document).ready(function () {
         var v3 = parseFloat($(".value3").val());
         var v4 = parseFloat($(".value4").val());
         console.log(v1 + "  -  " + v2);
-        $(".sum").html(v1 + v2 + v3 + v4 +" p.");
+        $(".sum").val(v1 + v2 + v3 + v4 +" p.");
         return false;
 
     });
@@ -64,14 +63,14 @@ $(document).ready(function () {
         var v3 = parseFloat($(".value3").val());
         var v4 = parseFloat($(".value4").val());
         console.log(v1 + "  +  " + v2);
-        $(".sum").html(v1 + v2 + v3 + v4 +" p.");
+        $(".sum").val(v1 + v2 + v3 + v4 +" p.");
         return false;
     });
     var v1 = parseFloat($(".value1").val());
     var v2 = parseFloat($(".value2").val());
     var v3 = parseFloat($(".value3").val());
     var v4 = parseFloat($(".value4").val());
-    $(".sum").html(v1 + v2 + v3 + v4 +" p.");
+    $(".sum").val(v1 + v2 + v3 + v4 +" p.");
 
 
     /* banner slick*/
