6d105cceb156bd9914f623dee409ec6980e0e028
ArtemBrest <artoym.zan@gmail.com>
2020-04-23 17:42:45 +0300

    1

--- js/script.js
+++ js/script.js
@@ -176,7 +176,7 @@ $(document).ready(function () {
                 breakpoint: 600,
                 settings: {
                     slidesToShow: 2,
-                    slidesToScroll: 1
+                    slidesToScroll: 2
                 }
             }
         ]
