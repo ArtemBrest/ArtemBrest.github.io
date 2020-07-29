367ba993402c22c9d672e67d433fc11ed4686fdf
ArtemBrest <artoym.zan@gmail.com>
2020-07-29 23:01:46 +0300

    1

--- js/script.js
+++ js/script.js
@@ -15,4 +15,31 @@ $(document).ready(function () {
             $(".portfolio_btn").html('Свернуть наши работы');
         }
     })
+    var btn_menu =  $(".header_mobile_menu_top").find('.icon');
+    var menu_ul = $(".header_mobile_menu_ul");
+    var fade = $('.menu-overlay');
+    btn_menu.click(function () {
+        if (menu_ul.hasClass('active')){
+            $('.header_mobile_menu').removeClass("overlay");
+            menu_ul.hide(350);
+            menu_ul.removeClass("active");
+            btn_menu.removeClass("active");
+            fade.hide(350);
+        }
+        else{
+            $('.header_mobile_menu').addClass("overlay");
+            menu_ul.addClass('active');
+            menu_ul.show(350);
+            btn_menu.addClass("active");
+            fade.show(350);
+        }
+
+    })
+    fade.click(function () {
+        $('.header_mobile_menu').removeClass("overlay");
+        menu_ul.hide(350);
+        menu_ul.removeClass("active");
+        btn_menu.removeClass("active");
+        fade.hide(350);
+    })
 });
