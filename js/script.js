5ceb6937972332409be2881bb2e24c2aaffb0cf6
ArtemBrest <artoym.zan@gmail.com>
2020-08-05 12:01:05 +0300

    1

--- js/script.js
+++ js/script.js
@@ -1,3 +1,21 @@
 $(document).ready(function () {
-
+    var icon = $(".header_contact_left-icon").find(".icon");
+    var overlay = $(".overlay-menu");
+    icon.click(function () {
+        if(icon.hasClass("active")){
+            icon.removeClass("active");
+            $(".header_mobile_menu").hide(350);
+            overlay.hide(350);
+        }
+        else{
+            icon.addClass("active");
+            overlay.show(350);
+            $(".header_mobile_menu").show(350);
+        }
+    });
+    overlay.click(function () {
+        icon.removeClass("active");
+        $(".header_mobile_menu").hide(350);
+        overlay.hide(350);
+    })
 });
