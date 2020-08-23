57c0fb86c699a7d4c96c3e889709f68cdd2dc0ac
ArtemBrest <artoym.zan@gmail.com>
2020-08-23 17:01:52 +0300

    2

--- js/script.js
+++ js/script.js
@@ -9,6 +9,43 @@ $(document).ready(function () {
         prevArrow: '<div class="sl_left_arrow"><img src="img/svg/left.svg" alt="" class="sl_left_arrow-icon"></div>',
         nextArrow: '<div class="sl_right_arrow"><img src="img/svg/right.svg" alt="" class="sl_right_arrow-icon"></div>  ',
         slidesToShow: 1,
-        slidesToScroll: 1,
+        responsive: [
+            {
+                breakpoint: 992,
+                settings: {
+                    arrows: false,
+                    centerMode: true
+                }
+            },
+            {
+                breakpoint: 768,
+                settings: {
+                    arrows: false,
+                    centerMode: true
+                }
+            },
+            {
+                breakpoint: 480,
+                settings: {
+                    arrows: false,
+                    centerMode: true
+                }
+            }
+        ]
     });
+
+    var btn = $(".partners_mob-btn");
+    var ul = $(".partners_mob_ul");
+    btn.click(function () {
+        if (btn.hasClass("active")){
+            btn.removeClass("active");
+            btn.html("Показать всех заказчиков");
+            ul.slideToggle(700);
+        }
+        else{
+            btn.addClass("active");
+            btn.html("Свернуть");
+            ul.slideToggle(700);
+        }
+    })
 });
