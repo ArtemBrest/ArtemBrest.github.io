4a7ad4fdef6c17dd63d1906a859679e22b84d3ed
ArtemBrest <artoym.zan@gmail.com>
2020-10-16 20:13:54 +0300

    123

--- js/script.js
+++ js/script.js
@@ -1,274 +1,14 @@
 $(document).ready(function () {
 
-    /*banner select*/
-    $('#form_select').each(function(){
-        // Variables
-        var $this = $(this),
-            selectOption = $this.find('option'),
-            selectOptionLength = selectOption.length,
-            selectedOption = selectOption.filter(':selected'),
-            dur = 500;
-
-        $this.hide();
-        // Wrap all in select box
-        $this.wrap('<div class="select"></div>');
-        // Style box
-        $('<div>',{
-            class: 'select__gap',
-            text: 'Выберите вид уборки',
-        }).insertAfter($this);
-
-
-        var selectGap = $this.next('.select__gap'),
-            caret = selectGap.find('.caret');
-        // Add ul list
-        $('<ul>',{
-            class: 'select__list'
-        }).insertAfter(selectGap);
-
-        var selectList = selectGap.next('.select__list');
-        // Add li - option items
-        for(var i = 0; i < selectOptionLength; i++){
-            $('<li>',{
-                class: 'select__item',
-                html: $('<span>',{
-                    text: selectOption.eq(i).text()
-                })
-            })
-                .attr('data-value', selectOption.eq(i).val())
-                .appendTo(selectList);
-        }
-        // Find all items
-        var selectItem = selectList.find('li');
-
-        selectList.slideUp(0);
-        $('.select__gap').append('<img src="img/png/Polygon.png" alt="" class="select__gap-img">')
-        selectGap.on('click', function(){
-            if(!$(this).hasClass('on')){
-                $(this).addClass('on');
-                selectList.slideDown(dur);
-                selectItem.on('click', function(){
-                    var chooseItem = $(this).data('value');
-
-                    $('select').val(chooseItem).attr('selected', 'selected');
-                    selectGap.text($(this).find('span').text());
-
-                    selectList.slideUp(dur);
-                    selectGap.removeClass('on');
-                    $('.select__gap').append('<img src="img/png/Polygon.png" alt="" class="select__gap-img">')
-                });
-
-            } else {
-                $(this).removeClass('on');
-                selectList.slideUp(dur);
-            }
-        });
-
+    $("#project_cards_preview").lightGallery({
+        selector: '.card'
     });
-    $('#form_select1').each(function(){
-        // Variables
-        var $this = $(this),
-            selectOption = $this.find('option'),
-            selectOptionLength = selectOption.length,
-            selectedOption = selectOption.filter(':selected'),
-            dur = 500;
-
-        $this.hide();
-        // Wrap all in select box
-        $this.wrap('<div class="select"></div>');
-        // Style box
-        $('<div>',{
-            class: 'select___gap',
-            text: 'Выберите услугу',
-        }).insertAfter($this);
-        $this
-
-
-        var selectGap = $this.next('.select___gap'),
-            caret = selectGap.find('.caret');
-        // Add ul list
-        $('<ul>',{
-            class: 'select___list'
-        }).insertAfter(selectGap);
-
-        var selectList = selectGap.next('.select___list');
-        // Add li - option items
-        for(var i = 0; i < selectOptionLength; i++){
-            $('<li>',{
-                class: 'select___item',
-                html: $('<span>',{
-                    text: selectOption.eq(i).text()
-                })
-            })
-                .attr('data-value', selectOption.eq(i).val())
-                .appendTo(selectList);
-        }
-        // Find all items
-        var selectItem = selectList.find('li');
-
-        selectList.slideUp(0);
-        $('.select___gap').append('<img src="img/png/Polygon.png" alt="" class="select___gap-img">')
-        selectGap.on('click', function(){
-            if(!$(this).hasClass('on')){
-                $(this).addClass('on');
-                selectList.slideDown(dur);
-                selectItem.on('click', function(){
-                    var chooseItem = $(this).data('value');
-
-                    $('select').val(chooseItem).attr('selected', 'selected');
-                    selectGap.text($(this).find('span').text());
-
-                    selectList.slideUp(dur);
-                    selectGap.removeClass('on');
-                    $('.select___gap').append('<img src="img/png/Polygon.png" alt="" class="select___gap-img">')
-                });
-
-            } else {
-                $(this).removeClass('on');
-                selectList.slideUp(dur);
-            }
-        });
-
-    });
-
-    /*conact select*/
-    $('#contact_form_select').each(function(){
-        // Variables
-        var $this = $(this),
-            selectOption = $this.find('option'),
-            selectOptionLength = selectOption.length,
-            selectedOption = selectOption.filter(':selected'),
-            dur = 500;
-
-        $this.hide();
-        // Wrap all in select box
-        $this.wrap('<div class="select"></div>');
-        // Style box
-        $('<div>',{
-            class: 'select__gap-contact',
-            text: 'Выберите вид уборки',
-        }).insertAfter($this);
-        $this
-
-
-        var selectGap = $this.next('.select__gap-contact'),
-            caret = selectGap.find('.caret');
-        // Add ul list
-        $('<ul>',{
-            class: 'select__list-contact'
-        }).insertAfter(selectGap);
-
-        var selectList = selectGap.next('.select__list-contact');
-        // Add li - option items
-        for(var i = 0; i < selectOptionLength; i++){
-            $('<li>',{
-                class: 'select__item-contact',
-                html: $('<span>',{
-                    text: selectOption.eq(i).text()
-                })
-            })
-                .attr('data-value', selectOption.eq(i).val())
-                .appendTo(selectList);
-        }
-        // Find all items
-        var selectItem = selectList.find('li');
-
-        selectList.slideUp(0);
-        $('.select__gap-contact').append('<img src="img/png/Polygon.png" alt="" class="select__gap-contact-img">')
-        selectGap.on('click', function(){
-            if(!$(this).hasClass('on')){
-                $(this).addClass('on');
-                selectList.slideDown(dur);
-                selectItem.on('click', function(){
-                    var chooseItem = $(this).data('value');
-
-                    $('select').val(chooseItem).attr('selected', 'selected');
-                    selectGap.text($(this).find('span').text());
-
-                    selectList.slideUp(dur);
-                    selectGap.removeClass('on');
-                    $('.select__gap-contact').append('<img src="img/png/Polygon.png" alt="" class="select__gap-contact-img">')
-                });
-
-            } else {
-                $(this).removeClass('on');
-                selectList.slideUp(dur);
-            }
-        });
-
-    });
-    $('#contact_form_select1').each(function(){
-        // Variables
-        var $this = $(this),
-            selectOption = $this.find('option'),
-            selectOptionLength = selectOption.length,
-            selectedOption = selectOption.filter(':selected'),
-            dur = 500;
-
-        $this.hide();
-        // Wrap all in select box
-        $this.wrap('<div class="select"></div>');
-        // Style box
-        $('<div>',{
-            class: 'select___gap-contact',
-            text: 'Выберите услугу',
-        }).insertAfter($this);
-        $this
-
-
-        var selectGap = $this.next('.select___gap-contact'),
-            caret = selectGap.find('.caret');
-        // Add ul list
-        $('<ul>',{
-            class: 'select___list-contact'
-        }).insertAfter(selectGap);
-
-        var selectList = selectGap.next('.select___list-contact');
-        // Add li - option items
-        for(var i = 0; i < selectOptionLength; i++){
-            $('<li>',{
-                class: 'select___item-contact',
-                html: $('<span>',{
-                    text: selectOption.eq(i).text()
-                })
-            })
-                .attr('data-value', selectOption.eq(i).val())
-                .appendTo(selectList);
-        }
-        // Find all items
-        var selectItem = selectList.find('li');
-
-        selectList.slideUp(0);
-        $('.select___gap-contact').append('<img src="img/png/Polygon.png" alt="" class="select___gap-contact-img">')
-        selectGap.on('click', function(){
-            if(!$(this).hasClass('on')){
-                $(this).addClass('on');
-                selectList.slideDown(dur);
-                selectItem.on('click', function(){
-                    var chooseItem = $(this).data('value');
-
-                    $('select').val(chooseItem).attr('selected', 'selected');
-                    selectGap.text($(this).find('span').text());
-
-                    selectList.slideUp(dur);
-                    selectGap.removeClass('on');
-                    $('.select___gap-contact').append('<img src="img/png/Polygon.png" alt="" class="select___gap-contact-img">')
-                });
-
-            } else {
-                $(this).removeClass('on');
-                selectList.slideUp(dur);
-            }
-        });
-
-    });
-
 
     /* accordion*/
     var accord = $(".accordion");
     var up = $(".icon");
-    accord.find(".accordion_header:not(.active)").siblings($(".accordion_content")).slideUp();
-    accord.find(".accordion_header").on("click", function () {
+    accord.find(".accordion_top:not(.active)").siblings($(".accordion_content")).slideUp();
+    accord.find(".accordion_top").on("click", function () {
         $(this).siblings($(".accordion_content")).stop().slideToggle(500);
         //$(this).parent(accord).find(".accordion_content").addClass("active")
         if ($(this).hasClass("active")) {
@@ -276,33 +16,172 @@ $(document).ready(function () {
         } else {
             $(this).addClass("active");
         }
-        if ($(this).find(".icon").hasClass("icon_active")) {
-            $(this).find(".icon").removeClass("icon_active");
+        if ($(this).find(".accordion_top-span").hasClass("active")) {
+            $(this).find(".accordion_top-span").removeClass("active");
         } else {
-            $(this).find(".icon").addClass("icon_active");
+            $(this).find(".accordion_top-span").addClass("active");
         }
     });
     /*end accordion*/
 
-   /* Menu*/
-    var btnMenu = $(".header_menu_desktop-icon");
+    /* Mob menu */
+    var btnMenu = $(".header_top_mobile_block");
     var menu = $(".header_menu_mobile");
     var overlay = $(".menu-overlay");
     btnMenu.click(function () {
         if (btnMenu.hasClass("active")){
             btnMenu.removeClass("active");
-            menu.hide(350);
-            overlay.hide(350);
+            menu.fadeOut(350);
+            overlay.fadeOut(350);
         }
         else{
             btnMenu.addClass("active");
-            menu.show(350);
-            overlay.show(350);
+            menu.fadeIn(350);
+            overlay.fadeIn(350);
         }
     });
     overlay.click(function () {
-        menu.hide(350);
+        menu.fadeOut(350);
         btnMenu.removeClass("active");
-        overlay.hide(350);
+        overlay.fadeOut(350);
+    });
+    /* end Mob menu */
+
+    /*masked input*/
+    jQuery(function ($) {
+        $(".modal1_form-input-phone").mask("+1-999-999-9999");
+    });
+    /* end masked input*/
+
+    /* my jquery submit*/
+    (function( $ ) {
+        $.fn.myPlugin = function() {
+            $("#fade").fadeIn(350);
+            $("#mainModal1").fadeIn(350);
+            $('#modal1_form').validate({
+                rules: {
+                    name: {
+                        required: true,
+                        minlength: 2,
+                    },
+                    phone: {
+                        required: true,
+                        minlength: 10,
+                    },
+
+                },
+                messages: {
+                    name: {
+                        required: ' ',
+                        minlength: ' '
+                    },
+                    phone: {
+                        required: ' ',
+                        minlength: ' '
+                    },
+
+                },
+                submitHandler: (function (form) {
+                    var name = $(".modal1_form-input-name").val(),
+                        phone = $('.modal1_form-input-phone').val(),
+                        message = $(".modal1_form-textarea").val()
+                    var options = {
+                        success: function (form) {
+                            $('#modal1_form')[0].reset();
+                            $("#mainModal1").fadeOut(350);
+                            $(".modal").fadeIn(350);
+                        },
+                        type: "POST",
+                        url: "/wp-admin/admin-ajax.php",
+                        data: ({
+                            action: 'plan_form',
+                            name: name,
+                            phone: phone,
+                            message: message
+                        }),
+
+                    };
+                    $(form).ajaxSubmit(options);
+                })
+            })
+        };
+    })(jQuery);
+    /* end my jquery submit*/
+
+    $(function () {
+        $('.banner-btn').on("click", function () {
+            // validate and process form here
+            $(this).myPlugin();
+        });
+        $('.banner_line_group-btn').on("click", function () {
+            // validate and process form here
+            $(this).myPlugin();
+        });
+        $('.step_cards .card-btn').on("click", function () {
+            // validate and process form here
+            $(this).myPlugin();
+        });
+    });
+    $(".fade").click(function () {
+        $(".modal").fadeOut(350);
+        $(".modal1").fadeOut(350);
+        $(".fade").fadeOut(350);
+    })
+    $(".modal-btn-close").click(function () {
+        $(".modal").fadeOut(350);
+        $(".fade").fadeOut(350);
+    })
+    $(".mainModal-close").click(function () {
+        $(".modal").fadeOut(350);
+        $(".modal1").fadeOut(350);
+        $(".fade").fadeOut(350);
+    })
+    $("#mainForm-btn").click(function () {
+        $('#mainForm').validate({
+            rules: {
+                name: {
+                    required: true,
+                    minlength: 2,
+                },
+                email: {
+                    required: true,
+                    minlength: 5,
+                },
+
+            },
+            messages: {
+                name: {
+                    required: ' ',
+                    minlength: ' '
+                },
+                email: {
+                    required: ' ',
+                    minlength: ' '
+                },
+
+            },
+            submitHandler: (function (form) {
+                var name = $(".form_group_line-input-name").val(),
+                    email = $('.form_group_line-input-email').val(),
+                    message = $(".form_group_line-textarea").val()
+                var options = {
+                    success: function (form) {
+                        $('#mainForm')[0].reset();
+                        $(".modal").fadeIn(350);
+                        $("#fade").fadeIn(350);
+                    },
+                    type: "POST",
+                    url: "/wp-admin/admin-ajax.php",
+                    data: ({
+                        action: 'plan_form',
+                        name: name,
+                        email: email,
+                        message: message
+                    }),
+
+                };
+                $(form).ajaxSubmit(options);
+            })
+        })
     })
 });
