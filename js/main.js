0c2cdf71798af8c72c78fe656261e564b6727d76
ArtemBrest <artoym.zan@gmail.com>
2021-03-25 22:26:21 +0300

    1232

--- js/main.js
+++ js/main.js
@@ -7,4 +7,20 @@ $(document).ready(function () {
         nextArrow: '<div class="sl_right_arrow"><img src="img/svg/reviews_bottom.svg" alt="" class="sl_right_arrow-icon"></div>  ',
         dots: true,
     });
+    var accord = $(".head_content_course_mob .course_item");
+    accord.find(".course_item_name:not(.active)").siblings("div").slideUp();
+    accord.find(".course_item_name").on("click", function () {
+        $(this).siblings("div").stop().slideToggle(500);
+
+        if ($(this).hasClass("active")) {
+            $(this).removeClass("active");
+        } else {
+            $(this).addClass("active");
+        }
+        if ($(this).find(".course_item-icon").hasClass("icon_active")) {
+            $(this).find(".course_item-icon").removeClass("icon_active");
+        } else {
+            $(this).find(".course_item-icon").addClass("icon_active");
+        }
+    });
 });
