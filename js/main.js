6de827ca3147e44589fc7efdc8709a2d3bb2f5d7
ArtemBrest <artoym.zan@gmail.com>
2021-03-27 14:33:36 +0300

    1244

--- js/main.js
+++ js/main.js
@@ -23,4 +23,102 @@ $(document).ready(function () {
             $(this).find(".course_item-icon").addClass("icon_active");
         }
     });
+
+
+    var btnMenu = $(".header_btn");
+    var menu = $(".header_menu");
+    var overlay = $(".menu-overlay");
+    btnMenu.click(function () {
+        menu.show(350);
+        overlay.show(350);
+    });
+    $(".header_menu-close").click(function () {
+        menu.hide(350);
+        overlay.hide(350);
+    })
+    overlay.click(function () {
+        menu.hide(350);
+        btnMenu.removeClass("active");
+        overlay.hide(350);
+    });
+
+    $('.Form_form').validate({
+        rules: {
+            name: {
+                required: true,
+                minlength: 2
+            },
+            email: {
+                required: true,
+            },
+            phone: {
+                required: true,
+                minlength: 10,
+            },
+
+        },
+        messages: {
+            name: {
+                required: 'Это поле являеться обязательным',
+                minlength: ' '
+            },
+            email: {
+                required: 'Это поле являеться обязательным'
+            },
+            phone: {
+                required: 'Это поле являеться обязательным',
+                minlength: 'Не вірно введено номер. Номер має містити 10 цифр.'
+            },
+
+        }
+    });
+    $('.registration_form').validate({
+        rules: {
+            name: {
+                required: true,
+                minlength: 2
+            },
+            email: {
+                required: true,
+            },
+            phone: {
+                required: true,
+                minlength: 10,
+            },
+
+        },
+        messages: {
+            name: {
+                required: 'Это поле являеться обязательным',
+                minlength: ' '
+            },
+            email: {
+                required: 'Это поле являеться обязательным'
+            },
+            phone: {
+                required: 'Это поле являеться обязательным',
+                minlength: 'Не вірно введено номер. Номер має містити 10 цифр.'
+            },
+
+        }
+    });
+    jQuery(function ($) {
+        $(".Form_form_input_phone").mask("+3 8(999) 999 99 99");
+    });
+
+    var accord = $(".course_accordion .accordion");
+    accord.find(".accordion_top:not(.active)").siblings("div").slideUp();
+    accord.find(".accordion_top").on("click", function () {
+        $(this).siblings("div").stop().slideToggle(500);
+        if ($(this).hasClass("active")) {
+            $(this).removeClass("active");
+        } else {
+            $(this).addClass("active");
+        }
+        if ($(this).find(".course_item-icon").hasClass("icon_active")) {
+            $(this).find(".course_item-icon").removeClass("icon_active");
+        } else {
+            $(this).find(".course_item-icon").addClass("icon_active");
+        }
+    });
 });
