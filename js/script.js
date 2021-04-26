dd6ab516ca9c99d04b418ca6ff7b7963662efd85
ArtemBrest <artoym.zan@gmail.com>
2021-04-26 20:19:34 +0300

    12

--- js/script.js
+++ js/script.js
@@ -1,5 +1,4 @@
 //"use strict";
-
 function fadeOut(el) {
     el.style.opacity = 1;
     (function fade() {
@@ -56,16 +55,56 @@ lightGallery(document.getElementById('portfolio_cards'),
     { selector: '.card', }
 );
 
-
-
 const smoothLinks = document.querySelectorAll('a[href^="#"]');
 for (let smoothLink of smoothLinks) {
     smoothLink.addEventListener('click', function (e) {
         e.preventDefault();
         const id = smoothLink.getAttribute('href');
+        for (i = 0; i < menu.length; i++) {
+            fadeOut(menu[i]);
+        }
+        for (i = 0; i < btnMenu.length; i++) {
+            btnMenu[i].classList.remove("active");
+        }
+        fadeOut(overlay);
         document.querySelector(id).scrollIntoView({
             behavior: 'smooth',
             block: 'start'
         });
     });
 };
+
+
+
+let btnMenu = document.getElementsByClassName("header_top_icon");
+let menu = document.getElementsByClassName("header_mob_menu");
+let overlay = document.getElementById("menu-overlay");
+let menu_link = document.getElementsByClassName("header_mob_menu_link");
+[].forEach.call(btnMenu,function(el){
+    el.addEventListener('click', function (e) {
+        if (this.classList.contains('active')){
+            this.classList.remove("active");
+            fadeOut(overlay);
+            for (i = 0; i < menu.length; i++) {
+                fadeOut(menu[i]);
+            }
+        }
+        else{
+            this.classList.add("active");
+            fadeIn(overlay,"block");
+            for (i = 0; i < menu.length; i++) {
+                fadeIn(menu[i],'block');
+            }
+        }
+    });
+});
+
+overlay.addEventListener('click', function (e) {
+    for (i = 0; i < menu.length; i++) {
+        fadeOut(menu[i]);
+    }
+    for (i = 0; i < btnMenu.length; i++) {
+        btnMenu[i].classList.remove("active");
+    }
+    fadeOut(overlay);
+});
