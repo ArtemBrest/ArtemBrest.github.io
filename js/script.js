08a51947d6372da806bf392c47655b984df6f72a
ArtemBrest <artoym.zan@gmail.com>
2021-06-04 18:53:52 +0300

    12345678

--- js/script.js
+++ js/script.js
@@ -24,49 +24,99 @@ $(document).ready(function () {
     $('.arrow-prev').on('click', function() {
         $('.pagesSlider').slick('slickPrev');
     });
-    if($(".intro_src").hasClass("left")){
-        $(".screen_compact").hide();
-        $(".screen_center").hide();
-    }
-    else if($(".intro_src").hasClass("compact")){
-        $(".screen_center").hide();
-        $(".screen_left").hide();
-    }
-    else{
-        $(".screen_left").hide();
-        $(".screen_compact").hide();
-    }
-    $(window).resize(function() {
-        if($(window).width() <= 845 ){
-            if($(".intro_src").hasClass("left")){
-                $(".screen_compact").hide();
-                $(".screen_left").hide();
-                $(".screen_center").show();
-                $(".intro_src").removeClass("left");
-            }
-            else if($(".intro_src").hasClass("compact")){
-                $(".screen_compact").hide();
-                $(".screen_left").hide();
-                $(".screen_center").show();
-                $(".intro_src").removeClass("compact");
-            }
-            $('.pagesSlider').slick('slickSetOption', 'adaptiveHeight', true, true);
+    $(".time-input").mask('99  :  99');
+
+    $('.relative_x').click(function () {
+        $(this).find('.polygon_x').toggleClass('rotateArrow');
+        $(this).find('.select__gap').toggleClass('whiteSelect');
+        $(this).toggleClass('whiteSelectBackground');
+    });
+
+    $(document).mouseup(function (e) {
+        // событие клика по веб-документу
+        var div = $('.select__list'); // тут указываем ID элемента
+        var div1 = $('.relative_x');
+        if (
+            !div1.is(e.target) && // если клик был не по нашему блоку
+            div1.has(e.target).length === 0
+        ) {
+            // и не по его дочерним элементам
+            div.slideUp(250); // скрываем его
+            $('.select__gap').removeClass('whiteSelect');
+            $('.relative_x').removeClass('whiteSelectBackground');
+            $('.select__gap').removeClass('on');
+            $('.polygon_x').removeClass('rotateArrow');
         }
     });
-    if($(window).width() <= 845 ){
-        if($(".intro_src").hasClass("left")){
-            console.log(1)
-            $(".screen_compact").hide();
-            $(".screen_left").hide();
-            $(".screen_center").show();
-            $(".intro_src").removeClass("left");
+    $('.select_item_x').each(function () {
+        // Variables
+        var $this = $(this),
+            selectOption = $this.find('option'),
+            selectOptionLength = selectOption.length,
+            selectedOption = selectOption.filter(':selected'),
+            dur = 250;
+
+        $this.hide();
+        // Wrap all in select box
+        $this.wrap('<div class="select"></div>');
+        // Style box
+        $('<div>', {
+            class: 'select__gap',
+            text: selectOption.eq(0).text(),
+        }).insertAfter($this);
+
+        var selectGap = $this.next('.select__gap'),
+            caret = selectGap.find('.caret');
+        // Add ul list
+        $('<ul>', {
+            class: 'select__list',
+        }).insertAfter(selectGap);
+
+        var selectList = selectGap.next('.select__list');
+        // Add li - option items
+        for (var i = 0; i < selectOptionLength; i++) {
+            $('<li>', {
+                class: 'select__item',
+                html: $('<span>', {
+                    text: selectOption.eq(i).text(),
+                }),
+            })
+                .attr('data-value', selectOption.eq(i).val())
+                .appendTo(selectList);
         }
-        else if($(".intro_src").hasClass("compact")){
-            $(".screen_compact").hide();
-            $(".screen_left").hide();
-            $(".screen_center").show();
-            $(".intro_src").removeClass("compact");
-        }
-        $('.pagesSlider').slick('slickSetOption', 'adaptiveHeight', true, true);
-    }
+        // Find all items
+        var selectItem = selectList.find('li');
+
+        selectList.slideUp(0);
+        selectGap.on('click', function () {
+            if (!$(this).hasClass('on')) {
+                $('.relative_x').removeClass('whiteSelectBackground');
+                $('.select__list').css('display', 'none');
+                selectList.slideDown(dur);
+                $('.select__gap').removeClass('on');
+                $(this).addClass('on');
+                $('.select__gap').removeClass('whiteSelect');
+                $('.polygon_x').removeClass('rotateArrow');
+
+                selectItem.on('click', function () {
+                    var chooseItem = $(this).data('value');
+
+                    $(this)
+                        .parent()
+                        .parent()
+                        .find('.select_item_x')
+                        .val(chooseItem)
+                        .attr('selected', 'selected');
+                    //$('select option:contains("Selection 1")')
+                    selectGap.text($(this).find('span').text());
+
+                    selectList.slideUp(dur);
+                    selectGap.removeClass('on');
+                });
+            } else {
+                $(this).removeClass('on');
+                selectList.slideUp(dur);
+            }
+        });
+    });
 });
