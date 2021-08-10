7681aec05d8580cd6c7b6130f30bade964d114af
unknown <artoym.zan@gmail.com>
2021-08-10 16:30:33 +0300

    DonKebab

--- js/script.js
+++ js/script.js
@@ -1,13 +1,3 @@
-function fadeOut(el) {
-    el.style.opacity = 1;
-    (function fade() {
-        if ((el.style.opacity -= .1) < 0) {
-            el.style.display = "none";
-        } else {
-            requestAnimationFrame(fade);
-        }
-    })();
-};
 // ** FADE IN FUNCTION **
 function fadeIn(el, display) {
     el.style.opacity = 0;
@@ -20,63 +10,53 @@ function fadeIn(el, display) {
         }
     })();
 };
+function fadeOut(el) {
+    el.style.opacity = 1;
+    (function fade() {
+        if ((el.style.opacity -= .1) < 0.1) {
+            el.style.display = "none";
+        } else {
+            requestAnimationFrame(fade);
+        }
+    })();
+};
 window.addEventListener("load", function(){
-    let fade = document.querySelector(".fade");
-    let modal_thanks = document.getElementById("modal_thanks");
-    let modal_thanks_close = document.querySelector(".modal-close");
-    let modal_thanks_btn = document.querySelector(".modal-btn-close");
-    let modal_form = document.getElementById("modal_form");
-    let modal_form_close = document.querySelector(".modal1-close");
-    let btn_modal = document.querySelectorAll(".btn_modal");
-    let btn_call = document.querySelector(".btn_call");
-    let title_modal = document.querySelector(".modal1_form-h2");
-    if (btn_call !== null) {
-        btn_call.addEventListener('click', event => {
-            title_modal.innerHTML = "Заказать звонок";
-            fadeIn(modal_form,'block');
-            fadeIn(fade,'block');
+    if (document.querySelector(".banner_swiper") !== null) {
+        var swiper = new Swiper(".banner_swiper", {
+            loop: true,
+            navigation: {
+                slidesPerView: 2,
+                nextEl: ".swiper-button-next",
+                prevEl: ".swiper-button-prev",
+            },
         });
     }
-    if (btn_modal !== null) {
-        for (let i = 0 ; i < btn_modal.length; i++){
-            btn_modal[i].addEventListener('click', event => {
-                title_modal.innerHTML = "Получить скидку";
-                fadeIn(modal_form,'block');
-                fadeIn(fade,'block');
-            });
+    let tab_link = document.querySelectorAll(".card_info_tab-link");
+    let tab_content = document.querySelectorAll(".tabcontent");
+    if (tab_link !== null || tab_content !== null) {
+        for(var i=0; i <tab_link.length; i++) {
+            (function(i) {
+                var link = tab_link[i];
+                link.onclick = function() {
+                    for(var j=0; j < tab_content.length; j++) {
+                        var display = window.getComputedStyle(tab_content[j]).display;
+                        if(display == "block") {
+                            link.classList.remove("active");
+                            fadeOut(tab_content[j]);
+                            tab_link[j].classList.remove("active");
+                        }
+                    }
+                    tab_link[i].classList.add("active");
+                    fadeIn(tab_content[i],"block");
+                }
+            })(i);
         }
     }
-    if (modal_thanks_btn !== null) {
-        modal_thanks_btn.addEventListener('click', event => {
-            fadeOut(modal_thanks);
-            fadeOut(modal_form);
-            fadeOut(fade);
-        });
-    }
-    if (modal_thanks_close !== null) {
-        modal_thanks_close.addEventListener('click', event => {
-            fadeOut(modal_thanks);
-            fadeOut(modal_form);
-            fadeOut(fade);
-        });
-    }
-    if (modal_form_close !== null) {
-        modal_form_close.addEventListener('click', event => {
-            fadeOut(modal_thanks);
-            fadeOut(modal_form);
-            fadeOut(fade);
-        });
-    }
-    if (fade !== null) {
-        fade.addEventListener('click', event => {
-            fadeOut(modal_thanks);
-            fadeOut(modal_form);
-            fadeOut(fade);
-        });
-    }
+
+
     let menu_overlay = document.querySelector(".menu-overlay");
     let mob_menu = document.querySelector(".header_mob_menu");
-    let mob_menu_icon = document.querySelector(".header_mob_top_btn");
+    let mob_menu_icon = document.querySelector(".header_info_mob");
     if(menu_overlay !== null){
         menu_overlay.addEventListener('click', event => {
             mob_menu_icon.classList.remove("active");
@@ -100,43 +80,20 @@ window.addEventListener("load", function(){
         });
     }
 
-    const preview_img = 6;
-    let category = document.querySelectorAll(".assortment_page_categories_cards");
-    let category_more_btn = document.querySelectorAll(".category_more-btn");
-    let number_all, number;
-    for (let i = 0 ; i < category.length; i++){
-        var card = category[i].querySelectorAll(".card");
-        number_all = category[i].querySelectorAll(".card").length;
-        number = number_all - preview_img;
-        if (number <= 0){
-            category[i].querySelector(".category_more-btn").style.display="none";
-        }
-        category[i].querySelector(".category_more-btn").querySelector(".number").innerHTML = number;
-        for (let i = 0 ; i < card.length; i++){
-            if (i >= preview_img){
-                fadeOut(card[i]);
-            }
-        }
+    const anchors = document.querySelectorAll('a[href*="#"]')
+
+    for (let anchor of anchors) {
+        anchor.addEventListener('click', function (e) {
+            e.preventDefault()
+            mob_menu_icon.classList.remove("active");
+            fadeOut(mob_menu);
+            fadeOut(menu_overlay);
+            const blockID = anchor.getAttribute('href').substr(1)
+            document.getElementById(blockID).scrollIntoView({
+                behavior: 'smooth',
+                block: 'start'
+            })
+        })
     }
-    for (let i = 0 ; i < category_more_btn.length; i++){
-        category_more_btn[i].addEventListener('click', event => {
-            var card = category_more_btn[i].parentNode.querySelectorAll(".card");
-            if(category_more_btn[i].classList.contains("active")){
-                category_more_btn[i].classList.remove("active");
-                category_more_btn[i].querySelector(".more").innerHTML = "Ещё + ";
-                for (let i = 0 ; i < card.length; i++){
-                    if (i >= preview_img){
-                        fadeOut(card[i]);
-                    }
-                }
-            }
-            else{
-                category_more_btn[i].classList.add("active");
-                category_more_btn[i].querySelector(".more").innerHTML = "Скрыть - ";
-                for (let i = 0 ; i < card.length; i++){
-                    fadeIn(card[i],"block");
-                }
-            }
-        });
-    }
+
 })
